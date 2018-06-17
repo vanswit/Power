d797966c35041c7494c38bf8de1bfddfa2575eac
Tom Vanswijgenhoven <Tom.Vanswijgenhoven@hotmail.com>
2018-06-17 17:57:51 +0200

    Add project files.

---
+++ Power/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Power.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
