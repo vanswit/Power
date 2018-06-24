497e342444477d64bb7c3f7be71561976504af58
Tom Vanswijgenhoven <Tom.Vanswijgenhoven@hotmail.com>
2018-06-24 18:28:30 +0200

    fyiuopspmzs

--- Power/Controllers/HomeController.cs
+++ Power/Controllers/HomeController.cs
@@ -12,5 +12,10 @@ namespace Power.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
     }
 }
--- Power/wwwroot/js/site.js
+++ Power/wwwroot/js/site.js
@@ -1,3 +1,18 @@
 ﻿// Write your JavaScript code.
 //var headerImageContainer = document.getElementById("header-image-container");
 //headerImageContainer.style.backgroundColor = "red";
+
+$(document).ready(function () {
+
+    $("#toggle-button").on("click", function () {
+
+        if ($("#toggle-button").val() == "See less") {
+            $("#toggle-button").val("See more");
+        }
+        else $("#toggle-button").val("See less");
+
+        $("p:last").toggle(100);
+    });
+
+});
+
