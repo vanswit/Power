6cb101252fca4285348c5953acfb962a12ae4691
Tom Vanswijgenhoven <Tom.Vanswijgenhoven@hotmail.com>
2018-07-16 15:59:35 +0200

    dzdazdz

---
+++ Power/Controllers/TransactionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Power.Models;
+
+namespace Power.Controllers
+{
+    public class TransactionController : Controller
+    {
+        public IActionResult Purchase(ProgramModel model)
+        {
+            return View();
+        }
+    }
+}
